0ff10ba2bbe2dbb220b8db0ce9449556bd9804e4
DESKTOP-DB9OINO\Alex <andrey.abramov2001@gmail.com>
2023-10-10 15:55:14 +0300

    Deleted garbage files (item, doors, composition) and relatives files

--- Assets/Item/I_Interactable.cs
+++
@@ -1,7 +0,0 @@
-
-public interface I_Interactable
-{
-    void OnHoverEnter();
-    void OnHoverExit();
-    void Interact();
-}
--- Assets/Player/Control/PlayerInputController.cs
+++ Assets/Player/Control/PlayerInputController.cs
@@ -1,25 +1,23 @@
 using UnityEngine;
 
+[RequireComponent(typeof(IControllable))]
 public class PlayerInputController : MonoBehaviour
 {
     private IControllable _controllable;
     private IViewable _viewable;
 
-    private Camera _mainCamera;
-    private I_Interactable _previousInteractable;
-
     private PlayerInput _playerInput;
 
     private void OnEnable()
     {
+        _playerInput.Enable();
         _playerInput.Gameplay.Jump.performed += OnJumpPerformed;
-        _playerInput.Gameplay.Interaction.performed += OnInteraction;
     }
 
     private void OnDisable()
     {
+        _playerInput.Disable();
         _playerInput.Gameplay.Jump.performed -= OnJumpPerformed;
-        _playerInput.Gameplay.Interaction.performed -= OnInteraction;
     }
 
     private void Awake()
@@ -27,21 +25,12 @@ public class PlayerInputController : MonoBehaviour
         _controllable = GetComponent<IControllable>();
         _viewable = GetComponentInChildren<IViewable>();
 
-        if(_controllable == null)
-        {
-            throw new System.Exception($"There is no IControllable component on the object: {gameObject.name}");
-        }
-
-        _mainCamera = GetComponentInChildren<Camera>();
-
         _playerInput = new PlayerInput();
-        _playerInput.Enable();
     }
 
     private void Update()
     {
         ReadMovement();
-        OnOutline();
     }
 
     private void LateUpdate()
@@ -66,52 +55,5 @@ public class PlayerInputController : MonoBehaviour
         var inputMousePosition = _playerInput.Gameplay.View.ReadValue<Vector2>();
 
         _viewable.Look(inputMousePosition);
-    }
-
-    private void OnOutline()
-    {
-        var mousePosition = _playerInput.Gameplay.OutlineItem.ReadValue<Vector2>();
-
-        Ray ray = _mainCamera.ScreenPointToRay(mousePosition);
-
-        if (Physics.Raycast(ray, out RaycastHit hit, 2.5f))
-        {
-            var interactable = hit.collider.GetComponent<I_Interactable>();
-
-            OutlineItem(interactable);
-        }
-        else
-        {
-            if(_previousInteractable != null)
-            {
-                _previousInteractable.OnHoverExit();
-                _previousInteractable = null;
-            }
-        }
-    }
-    
-    private void OutlineItem(I_Interactable interactable)
-    {
-        if (interactable != null)
-        {
-            if(interactable != _previousInteractable)
-            {
-                interactable.OnHoverEnter();
-                _previousInteractable = interactable;
-            }
-        }
-        else if (_previousInteractable != null)
-        {
-            _previousInteractable.OnHoverExit();
-            _previousInteractable = null;
-        }
-    }
-
-    private void OnInteraction(UnityEngine.InputSystem.InputAction.CallbackContext obj)
-    {
-        if(_previousInteractable != null)
-        {
-            _previousInteractable.Interact();
-        }
     }
 }
